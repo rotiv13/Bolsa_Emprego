d77d540bbc791b91b49bc073f201527f71be7898
Vitor Afonso <vitorafonso_32@hotmail.com>
2016-07-20 11:56:28 +0100

    Estrutura base de página estáticas implementada

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  #Retorna o titulo completo para cada página especifica
+  def full_title(page_title = ' ')
+    base_title = "Bolsa de Emprego"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " +base_title
+    end
+  end
+
 end
