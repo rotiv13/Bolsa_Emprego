df0d9bc64699d54e543384a2d905fb28baae991d
Vitor Afonso <vitorafonso_32@hotmail.com>
2016-07-29 18:32:22 +0100

    Tentativa de colocar as candidaturas a funcionar

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,8 +1,20 @@
 class Offer < ApplicationRecord
   belongs_to :user
+
+  has_many :offer_active_relationships, class_name: "OfferRelationship",
+           foreign_key: "user_fer_id",
+           dependent: :destroy
+
+  has_many :offer_passive_relationships, class_name: "OfferRelationship",
+           foreign_key: "offer_fed_id",
+           dependent: :destroy
+
+  has_many :offer_following, through: :offer_active_relationships, source: :offer_fed
+  has_many :offer_followers, through: :offer_passive_relationships, source: :user_fer
+
   default_scope -> { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
-  validates :user_id, presence: true
+  validates :user_follower_id, presence: true
   validates :description, presence: true
   validates :title, presence: true
   validates :date_begin, presence: true
@@ -17,6 +29,7 @@ class Offer < ApplicationRecord
       where("prof_area like '#{field}'")
     end
   end
+
 
   def activate
     update_attribute(:active, true)
--- db/seeds.rb
+++ db/seeds.rb
@@ -183,3 +183,5 @@ following = users[2..20]
 followers = users[3..40]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
+
+
