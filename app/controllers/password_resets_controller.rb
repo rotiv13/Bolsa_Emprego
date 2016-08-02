591b0283f8f293fc136f528e2876ca1a8d21de08
Vitor Afonso <vitorafonso_32@hotmail.com>
2016-08-02 18:17:28 +0100

    Adicao do f.obejct no formulário de reset da password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,10 +11,10 @@ class PasswordResetsController < ApplicationController
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
-      flash[:info] = "Email sent with password reset instructions"
+      flash[:info] = "Email enviado com instruções para o reset da password"
       redirect_to root_url
     else
-      flash.now[:danger] = "Email address not found"
+      flash.now[:danger] = "Email não encontrado"
       render 'new'
     end
   end
